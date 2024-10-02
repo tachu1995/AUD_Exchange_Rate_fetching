610633fbd77f7e7ab2dc7a60908287697794ef21
tachu1995 <dilapinc@gmail.com>
2024-10-02 23:39:24 +1000

    Update rate_fetching.py
    Define each of the HTTP request as its own function, creating a simple way to quit application.
    
    Fixing up log message for more clarity

--- rate_fetching.py
+++ rate_fetching.py
@@ -5,6 +5,9 @@ from google.oauth2.service_account import Credentials # Work with Google API aut
 from datetime import datetime # Work With Date and Time
 from googleapiclient.discovery import build # Building Google API Services
 import time # Use to perform delay, sleep
+from datetime import datetime, timedelta
+import threading
+
 
 # Set Google Sheets API scopes and credentials 
 scopes = ["https://www.googleapis.com/auth/spreadsheets"]
@@ -29,51 +32,90 @@ TECHCOMBANK_URL = "AAAAAAAAAAAAAAAAAAAAAAAAA"
 VIETCOMBANK_URL = "AAAAAAAAAAAAAAAAAAAAAAAAA"
 
 # Define payload for POST request to HH API
-payload = {"AAAAAAAAAAAAAAAAAAAAAAAAA"}
+payload = {"countryCode":"VNM","dmCode":"HOME","currencyCode":"VND"}
+
+# Fetching Ezy Remit Aud-VND Rate
+def ezy_aud_rate_fetching():
+    try:
+        rate = requests.get(EZY_URL)
+        return rate.json()["rate"]
+    except:
+        return "N/A"
+    
+# Fetching HH AUD-VND Rate
+def hh_aud_rate_fetching():
+    try:
+        rate = requests.post(json=payload, url=HH_URL)
+        return rate.json()["rateValue"]
+    except:
+        return "N/A"
+    
+# Fetching Techcombank AUD-VND Rate
+def tech_aud_rate_fetching():
+    try:
+        rate = requests.get(TECHCOMBANK_URL)
+        return rate.json()["exchangeRate"]["data"][0]["askRateTM"]
+    except:
+        return "N/A"
+
+# Fetching Vietcombank AUD-VND Rate
+def viet_aud_rate_fetching():
+    try:
+        rate = requests.get(VIETCOMBANK_URL)
+        # Parse the HTML content from the Vietcombank page
+        soup = BeautifulSoup(rate.content, 'html.parser')
+        # Find the <li> element with data-code="AUD"
+        aud_element = soup.find('li', {'data-code': 'AUD'})
+        return aud_element.get("data-sell-rate")
+    except:
+        return "N/A"
 
 def rate_fetching():
     # Making requests to the sites using requests library 
-    ezy_response = requests.get(EZY_URL)
-    hh_response = requests.post(json=payload, url=HH_URL)
-    tech_response = requests.get(TECHCOMBANK_URL)
-    viet_response = requests.get(VIETCOMBANK_URL)
-
+    hh_rate = hh_aud_rate_fetching()
+    ezy_rate = ezy_aud_rate_fetching()
+    tectcombank_rate = tech_aud_rate_fetching()
+    vietombank_rate = viet_aud_rate_fetching()
     
-    # Parse the HTML content from the Vietcombank page
-    soup = BeautifulSoup(viet_response.content, 'html.parser')
+    # Get Date and tinme 
+    date = datetime.now().strftime("%Y-%m-%d")
+    date_hour = datetime.now().strftime("%H:%M")
 
-    # Find the <li> element with data-code="AUD"
-    aud_element = soup.find('li', {'data-code': 'AUD'})
-    try:
-        tectcombank_rate = tech_response.json()["exchangeRate"]["data"][0]["askRateTM"]
-        vietombank_rate = aud_element.get("data-sell-rate")
-        hh_rate = hh_response.json()["rateValue"]
-        ezy_rate = ezy_response.json()["rate"]
-
-        # Get Date and tinme 
-        date = datetime.now().strftime("%Y-%m-%d %H:%M")
 
         # Prepare the data to be insert into Google Sheet
-        data = {
+    data = {
         "values": [
-            [date, hh_rate, ezy_rate, tectcombank_rate, vietombank_rate]
+            [date, date_hour, hh_rate, ezy_rate, tectcombank_rate, vietombank_rate]
         ]
         }
         
-        # Attemp to append the data into a new row of the Google Sheet, if failed will try again right away
-        try:
-            result = service.spreadsheets().values().append(spreadsheetId=sheet_id, range=range_name, valueInputOption='USER_ENTERED', body=data).execute()
-            print(f"{date}: Append to Google Sheet Successfully.")
-            # print(result)
-            # Sleep for 15 minutes between each call
-            time.sleep(900)
-        except:
-            print("Error: Failed to Append to Google Sheet")
+    # Attemp to append the data into a new row of the Google Sheet, if failed will try again right away
+    # print(data)
+    try:
+        result = service.spreadsheets().values().append(spreadsheetId=sheet_id, range=range_name, valueInputOption='USER_ENTERED', body=data).execute()
+        # Calculate the time for the next call (5 minutes later)
+        next_call_time = (datetime.now() + timedelta(minutes=15)).strftime("%H:%M")
+        print(f"{date} {date_hour}: Append to Google Sheet Successfully, next call {next_call_time}")
+        # print(result)
+        # Sleep for 15 minutes between each call
+        time.sleep(900)
     except:
-        print("Error: Failed to fetch data from sites")
+        print("Error: Failed to Append to Google Sheet")
         
+        
+
+def stop_loop():
+    input("Press Enter to stop...\n")
+    global stop
+    stop = True
+
         
 if __name__ == "__main__":
-    while True:
+    stop = False
+    # Create a thread to listen for the stop command
+    threading.Thread(target=stop_loop).start()
+
+    while not stop:
         rate_fetching()
         
+    print("Process stopped.")
